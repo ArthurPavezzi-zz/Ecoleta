bb91c9dc6e4a588506397e365f53b94486e129f2
Arthur Pavezzi <arthur.henrique.pavezzi@gmail.com>
2020-06-07 21:16:14 -0300

    Aula 4 Parte 3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const server = express();
 
+// configurando pasta pública
+server.use(express.static('public'));
+
+// utilizando template engine nunjucks
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+});
+
+// Configurar caminhos para a aplicação
+// página inicial
+server.get("/", (req, res) => {
+    return res.render('index.html')
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render('create-point.html')
+});
+
+server.get("/search", (req, res) => {
+    return res.render('search-results.html')
+});
+
 
 // Ligar o servidor 
 server.listen(3000);
