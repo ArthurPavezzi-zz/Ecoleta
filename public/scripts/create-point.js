9c8bd9ce14f24bc2bd7842df9a7f78a0338dd9bc
Arthur Pavezzi <arthur.henrique.pavezzi@gmail.com>
2020-06-07 22:33:02 -0300

    Finalização Aula 4

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,74 +1,80 @@
 function populateUFs() {
-    const ufSelect = document.querySelector("select[name=uf]");
-
-    // Em ordem alfabética
+    const ufSelect = document.querySelector("select[name=uf]")
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
-        .then( res => res.json() )
-        .then( states => {
+    .then( res => res.json() )
+    .then( states => {
+        for( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
 
-            for (state of states) {
-                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
-            }
+    })
+}
 
-})};
-
-populateUFs();  
+populateUFs()
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]");
-    const stateInput = document.querySelector("input[name=state]");
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
 
-    const ufValue = event.target.value;
+    const ufValue = event.target.value
 
-    const indexOfSelectState = event.target.selectedIndex;
-    stateInput.value = event.target.options[indexOfSelectState].text;
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
-    // Em ordem alfabética
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`;
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`
 
-    citySelect.innerHTML = '<option value="">Selecione a Cidade</option>';
-    citySelect.disabled = true;
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
 
     fetch(url)
-        .then( res => res.json() )
-        .then( cities => {
-            for( const city of cities ) {
-                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
-            }
-            citySelect.disabled = false;
-        })
-};
+    .then( res => res.json() )
+    .then( cities => {
+        
+
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
+}
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", getCities);
+    .addEventListener("change", getCities)
 
-const itemsToCollect = document.querySelectorAll(".items-grid li");
+const itemsToCollect = document.querySelectorAll(".items-grid li")
 
-for (let item of itemsToCollect) {
-    item.addEventListener("click", handleSelectedItem);
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
 }
 
 const collectedItems = document.querySelector("input[name=items]")
 
-let selectedItems = [];
+let selectedItems = []
 
 function handleSelectedItem(event) {
-    const itemLi = event.target;
-    itemLi.classList.toggle("selected");
-    const itemId = itemLi.dataset.id;
+    const itemLi = event.target
 
-    const alreadySelected = selectedItems.findIndex( item => { 
-        return item == itemId });    
-    if (alreadySelected >= 0){
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    })
+
+    if( alreadySelected >= 0 ) {
         const filteredItems = selectedItems.filter( item => {
-            return item != itemId
-        });
-        
-        selectedItems = filteredItems;
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
 
+        selectedItems = filteredItems
     } else {
-        selectedItems.push(itemId);
+        selectedItems.push(itemId)
     }
-    collectedItems.value = selectedItems;
-};
+
+    collectedItems.value = selectedItems
+
+}
--- src/server.js
+++ src/server.js
@@ -1,30 +1,24 @@
-const express = require("express");
-const server = express();
+const express = require("express")
+const server = express()
 
-// configurando pasta pública
-server.use(express.static('public'));
+server.use(express.static("public"))
 
-// utilizando template engine nunjucks
-const nunjucks = require('nunjucks');
-nunjucks.configure('src/views', {
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
     express: server,
-    noCache: true,
-});
+    noCache: true
+})
 
-// Configurar caminhos para a aplicação
-// página inicial
 server.get("/", (req, res) => {
-    return res.render('index.html')
-});
+    return res.render("index.html", { title: "Um título"})
+})
 
 server.get("/create-point", (req, res) => {
-    return res.render('create-point.html')
-});
+    return res.render("create-point.html")
+})
 
 server.get("/search", (req, res) => {
-    return res.render('search-results.html')
-});
+    return res.render("search-results.html")
+})
 
-
-// Ligar o servidor 
-server.listen(3000);
+server.listen(3000)
